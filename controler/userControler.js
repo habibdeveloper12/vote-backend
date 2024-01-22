7659d509696f86ca695b3eddb25e69e13e5c4e5d
habibur rahman <habibur.rahman@elementzerolabs.com>
2024-01-22 23:59:49 +0600

    sd

--- controler/userControler.js
+++ controler/userControler.js
@@ -58,14 +58,16 @@ exports.getUserByEmail = async (req, res, next) => {
 exports.postVote = async (req, res, next) => {
   try {
     const { voterid } = req.query;
-    const { voted } = req.body;
+    const body = req.body;
+    console.log(body);
     const updatedUser = await User.findOneAndUpdate(
       { voterid },
-      { $set: { vote: voted } }, // Use $set to update the specific field
-      { new: true } // Set new to true to get the updated user document in the response
+      { $set: { vote: body.vote } },
+      { new: true, upsert: true } // Combine options correctly
     );
-    console.log(updatedUser);
+
     res.json(updatedUser);
+    console.log(updatedUser);
   } catch (error) {
     console.error("Error fetching addresses:", error);
     res.status(500).json({ error: "Internal Server Error" });
